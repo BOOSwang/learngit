1a01d81de9bc6bdec8ee5f6c569219b4213f15d2
wangyanci <1973155911@qq.com>
2018-09-06 20:56:56 +0800

    测试提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,19 @@
+/**
+ * Created by wangyanci on 2018/9/6.
+ */
+
+/**
+ * &lt;pre&gt;项目名称：
+ * 类名称：
+ * 类描述：
+ * 创建人：王晏赐
+ * 创建时间：
+ * 修改人：王晏赐
+ * 修改时间：
+ * 修改备注：
+ *
+ * @version &lt;/pre&gt;
+ */
+
+public class Test {
+}
